7b2cdc17b222f54095717204183f2ab8e9aa25ad
Bobur Yusupov <86429857+dev-yusupov@users.noreply.github.com>
2024-11-09 12:00:57 +0100

    Create multiply_matrices.py

---
+++ functions/math/multiply_matrices.py
@@ -0,0 +1,17 @@
+def multiply(matrix1, matrix2):
+        
+    result = []
+    
+    for i in range(len(matrix1)):
+        res = []
+        for j in range(len(matrix2[0])):
+            c = 0
+            for k in range(len(matrix2)):
+                c += matrix1[i][k] * matrix2[k][j]
+            
+            
+            res.append(c)
+            
+        result.append(res)
+        
+    return result
